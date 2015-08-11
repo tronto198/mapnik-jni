6fc60ca7946ccbe3779f8854cdc162825af2ba2e
Julian Raschke <julian.raschke@natcon7.com>
2015-08-11 11:43:51 +0200

    Replace JNI lib loading logic w/ plain loadLibrary

--- test/mapnik/Setup.java
+++ test/mapnik/Setup.java
@@ -2,11 +2,7 @@ package mapnik;
 
 
 public class Setup {
-	private static boolean inited=false;
-	
 	public static void initialize() {
-		if (inited) return;
-		inited=true;
 		Mapnik.initialize();
 	}
 	
